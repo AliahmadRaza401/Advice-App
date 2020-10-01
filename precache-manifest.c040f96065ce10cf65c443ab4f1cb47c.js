7bf991848c923dcf707792c7ee0c10ba2fc68291
ali raza <aliahmadraza401@gmail.com>
2020-10-01 06:34:01 +0500

    Updates

---
+++ precache-manifest.c040f96065ce10cf65c443ab4f1cb47c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d7da5e1fc2054fbeb4bd03cf1563d2",
+    "url": "/Advice-App/index.html"
+  },
+  {
+    "revision": "475029b7e35fd9c268e4",
+    "url": "/Advice-App/static/css/main.9e5316cb.chunk.css"
+  },
+  {
+    "revision": "e65f4baa4412a3d97030",
+    "url": "/Advice-App/static/js/2.4aceca2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Advice-App/static/js/2.4aceca2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475029b7e35fd9c268e4",
+    "url": "/Advice-App/static/js/main.007ee077.chunk.js"
+  },
+  {
+    "revision": "9e3e056a3200dc6f4af8",
+    "url": "/Advice-App/static/js/runtime-main.849ce040.js"
+  },
+  {
+    "revision": "47ef084f5dd84aad59bc5a888780a857",
+    "url": "/Advice-App/static/media/p1.47ef084f.jpg"
+  },
+  {
+    "revision": "cd4b7fc1b07ff5c8d9437a1e5dd675ad",
+    "url": "/Advice-App/static/media/p2.cd4b7fc1.jpg"
+  }
+]);
